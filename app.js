1db69020ca2ba0cd82b30f20933d66ed83dc3f25
rohan <rohanrajgautam@gmail.com>
2020-06-26 23:14:23 +0545

    added show route to the blog app

--- app.js
+++ app.js
@@ -55,6 +55,16 @@ app.post('/blogs', (req, res) => {
   })
 })
 
+// SHOW ROUTE
+app.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+      res.render('show', { blog: foundBlog })
+    }
+  })
+})
 
 // LISTENING TO PORT
 app.listen(port, () => console.log(`server listening at https://localhost:${port}`));
